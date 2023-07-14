3d4f78eaa153673d95a01a434624a55087ea3d5f
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-07-14 15:01:48 +0200

    input management - controllo primary action

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,9 @@ extends Node2D
 #gestione input player
 const PLAYER_SPEED: int = 500
 
+# le azioni left, right, up, down, primary action e secondary action sono definite
+# all'interno del menu Project-> Project Settings -> Input Map
+
 func _process(delta):
 	
 	#Gets an input vector by specifying four actions for the positive and negative X and Y axes.
@@ -12,4 +15,6 @@ func _process(delta):
 	#aggiorna posiziione
 	position += direction * PLAYER_SPEED * delta
 	
-	
+	#controllo pressione azione
+	if Input.is_action_pressed("primary action"):
+		print("Shoot!")
