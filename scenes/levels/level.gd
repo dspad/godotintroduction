62e588252d1dbbf6081097598a6e6786563184cf
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-07-14 17:10:45 +0200

    PHYSICS - Aggiornato player in CharacterBody2D & rimozione logo

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,20 +1,20 @@
 extends Node2D
 
-func _ready():
-	$Logo.rotation_degrees = 90
+#func _ready():
+#	$Logo.rotation_degrees = 90
 	
 	#esempio accesso a un nodo usando nome univoco
 	#invece che $Player/Node2D/SubNode
-	%SubNode.position = Vector2.ZERO
+	#%SubNode.position = Vector2.ZERO
 
-func _process(delta):
-	$Logo.rotation_degrees += 60 * delta
+#func _process(delta):
+#	$Logo.rotation_degrees += 60 * delta
 	
-	if $Logo.rotation_degrees > 180:
-		$Logo.rotation_degrees = 0
+#	if $Logo.rotation_degrees > 180:
+#		$Logo.rotation_degrees = 0
 		
-	if $Logo.pos.x > 1000:
-		$Logo.pos = Vector2.ZERO
+#	if $Logo.pos.x > 1000:
+#		$Logo.pos = Vector2.ZERO
 
 func printAStringFromAnotherNode():
 	print("printAStringFromAnotherNode")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CharacterBody2D
 
 #gestione input player
 const PLAYER_SPEED: int = 500
@@ -17,8 +17,14 @@ func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	#print(direction)
 	
-	#aggiorna posiziione
-	position += direction * PLAYER_SPEED * delta
+	#muovi il player in quanto CharacterBody2D. Su velocity si impostano i valori direzionali
+	#che vengono poi usati in move_and_slide.
+	#Non si aggiunge il delta perchè viene usato direttamente in move_and_slide
+	velocity = direction * PLAYER_SPEED
+	move_and_slide()
+	
+	#aggiorna posiziione - vecchio codice
+	#position += direction * PLAYER_SPEED * delta
 	
 	#controllo pressione azione
 	if Input.is_action_pressed("primary action"):
