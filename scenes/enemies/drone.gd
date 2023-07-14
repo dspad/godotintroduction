bcf223a11c748fdcf3801def2d5da13eb3bd10a7
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-07-14 17:48:27 +0200

    PHYSICS - esercitazione

---
+++ scenes/enemies/drone.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+const DRONE_DIRECTION : Vector2 = Vector2.RIGHT
+const DRONE_SPEED : int = 200
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	#applica direzione e velocità
+	velocity = DRONE_DIRECTION * DRONE_SPEED
+	move_and_slide()
