05dbe54f419ef251391c9a0dbd8a6b0d78c3a548
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-07-20 21:25:02 +0200

    Gestione collisione laser con il livello

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -3,8 +3,13 @@ extends CharacterBody2D
 const DRONE_DIRECTION : Vector2 = Vector2.RIGHT
 const DRONE_SPEED : int = 200
 
+var is_enemy : bool = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	#applica direzione e velocità
 	velocity = DRONE_DIRECTION * DRONE_SPEED
 	move_and_slide()
+
+func hit():
+	print("damage")
--- scenes/projectile/laser.gd
+++ scenes/projectile/laser.gd
@@ -7,3 +7,22 @@ var local_laser_direction : Vector2 = Vector2.UP
 
 func _process(delta):
 	position += local_laser_direction * LASER_SPEED * delta
+
+
+func _on_body_entered(body):
+	#verifica se il parametro presenta un metodo specifico
+	#if body.has_method("hit"): #questa istruzione fa la stessa cosa
+	if "hit" in body:
+		body.hit()
+	
+	#è possibile anche verificare che in body esista una proprietà
+	#in questo caso verifica se esiste is_enemy
+	#if "is_enemy" in body:
+	#	print("body is an enemy")
+	
+	#distruggi il laser appena entra in collisione con un altro elemento
+	queue_free()
+
+
+func _on_timer_timeout():
+	queue_free()
