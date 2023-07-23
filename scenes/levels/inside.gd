84a5e040743f46e8244fb9c4a717d4748d93b8c5
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-07-23 18:21:43 +0200

    LEVEL TRANSITION - implementazione caricamento altra scena

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,7 +1,8 @@
 extends LevelParent
 
-
+@export var outside_level_scene : PackedScene = preload("res://scenes/levels/outside.tscn")
 
 
 func _on_exit_gate_area_body_entered(body):
-	print("player exited")
+	#caircamento scena da variabile
+	get_tree().change_scene_to_packed(outside_level_scene)
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -4,3 +4,7 @@ func _on_gate_player_entered_gate(object_name):
 	var tween = get_tree().create_tween()
 	#esempio di uso di tween su una proprietà dello script del player
 	tween.tween_property($Player, "speed",0,1)
+	
+	#caricamento della scena inside
+	#modalità di caricamento inserendo direttamente il percorso della scena
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
