7f1b0de14154c669feb062fd796ff2cfc764dc98
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-07-25 19:29:47 +0200

    fix ultimo esercizio per mantenere i valori in cambio livello

--- scenes/UI/ui.gd
+++ scenes/UI/ui.gd
@@ -4,6 +4,10 @@ extends CanvasLayer
 @onready var laser_label : Label = $LaserCounter/Bullets/Label
 @onready var grenade_label : Label = $GrenadeCounter/VBoxContainer/Label
 
+func _ready():
+	update_laser_text()
+	update_grenade_text()
+
 func update_laser_text():
 	#converti in stringa
 	laser_label.text = str(Globals.laser_amount)
