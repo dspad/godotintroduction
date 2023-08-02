1021ff75f37e8c02884000726668cfc560b1277a
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-08-02 16:00:20 +0200

    implementazione controllo su salute player

--- globals/globals.gd
+++ globals/globals.gd
@@ -17,6 +17,8 @@ var grenade_amount = 5:
 			grenade_amount = value
 			stat_changed.emit()
 
+var player_hitted : bool = false
+
 #esempio di utilizzo delle funzioni get e set che vengono richiamaate ogni volta
 #che si accede a questa variabile. Molto utile per richiamare funzioni collegate al suo
 #valore (esempio: aggiornamento UI)
@@ -24,5 +26,21 @@ var health_amount = 60:
 		#get:
 		#	return health_amount
 		set(value):
-			health_amount = value
+			#se il nuovo valore supera il precedente, aggiorna subito
+			if value > health_amount:
+				health_amount = min(value,100) #istruzione inserita nel caso in cui la salute sia al massimo
+			elif not player_hitted: #istruzioni in caso di danno subito
+				health_amount = value
+				player_hitted = true
+				player_hitted_timer()
 			stat_changed.emit()
+
+#variabile messa a disposizione per fare in modo che i nemici puntino verso il player
+var player_pos : Vector2
+
+
+#esempio di creazione di un timer direttamente via codice
+func player_hitted_timer():
+	#attendi la fine del timer per reimpostare la variabile a false
+	await get_tree().create_timer(3).timeout
+	player_hitted = false
