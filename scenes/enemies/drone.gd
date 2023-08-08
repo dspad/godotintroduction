f662a87924fdc573c52c7c9f0c81c5b28c0568ad
Daniele Spadoni <d.spadoni@bastetpaw.com>
2023-08-08 23:37:20 +0200

    Setup logica drone

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -1,15 +1,39 @@
 extends CharacterBody2D
 
-const DRONE_DIRECTION : Vector2 = Vector2.RIGHT
-const DRONE_SPEED : int = 200
+var active : bool = false
+var speed : int = 400
+var vulnerable : bool = true
+var health = 50
 
-var is_enemy : bool = true
-
+func _ready():
+	$Explosion.hide()
+	$Sprite2D.show()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#applica direzione e velocità
-	velocity = DRONE_DIRECTION * DRONE_SPEED
-	move_and_slide()
+	if active:
+		look_at(Globals.player_pos)
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			$AnimationPlayer.play("explosion")
 
 func hit():
-	print("damage")
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$Timers/HitTimer.start()
+	if health <= 0:
+		$AnimationPlayer.play("explosion")
+
+func _on_notice_area_body_entered(body):
+	active = true
+
+
+func _on_notice_area_body_exited(body):
+	active = false
+
+
+func _on_hit_timer_timeout():
+	vulnerable = true
